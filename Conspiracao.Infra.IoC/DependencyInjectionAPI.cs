801f619fee1fec0dba97fe78a2bb53b86df3577f
Leandro <leandro.almeida@uvvnet.com.br>
2024-06-13 13:28:21 -0300

    implementando a camada de infra Ioc para inversão das dependências

---
+++ Conspiracao.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,29 @@
+﻿using Conspiracao.Domain.Interfaces;
+using Conspiracao.Infra.Data.Context;
+using Conspiracao.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conspiracao.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfraEstructureAPI(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+            services.AddDbContext<ConspiracaoDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
+
+            return services;
+        }
+    }
+}
